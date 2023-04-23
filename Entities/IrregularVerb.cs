366203647f4c38a95859b81d3b2479aa6e4b2f2e
Egor Shaklein <eashaklein@gmail.com>
2023-04-23 11:32:59 +0400

    Created the main class

---
+++ Entities/IrregularVerb.cs
@@ -0,0 +1,16 @@
+﻿namespace TeacherEnglish.Entities
+{
+    public class IrregularVerb
+    {
+        public int Id { get; set; }
+
+        public string Infinitive { get; set; }
+
+        public string PastSimple { get; set; }
+
+        public string PastParticiple { get; set; }
+
+        public string Translation { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TeacherEnglish;
+
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory());
+builder.AddJsonFile("appsettings.json");
+var config = builder.Build();
+
+string connectionString = config.GetConnectionString("DefaultConnection");
+var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+var options = optionsBuilder.UseNpgsql(connectionString).Options;
+
+using (ApplicationContext db = new ApplicationContext(options))
+{
+    
+}
